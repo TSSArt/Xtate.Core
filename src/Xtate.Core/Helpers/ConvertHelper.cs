b848a25886237e1413e62dcf9ac01f354fd9be50
Sergii Artemenko <sergii.artemenko@tssart.com>
2024-06-29 12:10:26 +0200

    Chcek Ubuntu tests

--- src/Xtate.Core/Helpers/ConvertHelper.cs
+++ src/Xtate.Core/Helpers/ConvertHelper.cs
@@ -26,7 +26,8 @@ internal static class ConvertHelper<TFrom, TTo>
 	private static Func<TFrom, TTo> GetConverter()
 	{
 		var arg = Expression.Parameter(typeof(TFrom));
+		var body = Expression.Convert(arg, typeof(TTo));
 
-		return Expression.Lambda<Func<TFrom, TTo>>(arg, arg).Compile();
+		return Expression.Lambda<Func<TFrom, TTo>>(body, arg).Compile();
 	}
 }
